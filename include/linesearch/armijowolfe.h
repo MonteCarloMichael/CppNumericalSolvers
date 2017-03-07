6502ed4fc0a9c691566977f13264fd4eb3edf258
Michael Andree Heuer <mheuer@student.ethz.ch>
2017-03-07 17:08:57 +0100

    Implemented finite bracketing for armijo-wolfe linesearch.

--- include/linesearch/armijowolfe.h
+++ include/linesearch/armijowolfe.h
@@ -3,6 +3,7 @@
 #define ARMIJOWOLFE_H_
 
 #include "../meta.h"
+#include <cmath>
 
 namespace cppoptlib {
 
@@ -28,21 +29,21 @@ namespace cppoptlib {
 
       assert(searchDir.norm() != 0);
 
-      const Scalar c1 = 1e-4;
+      const Scalar c1 = 0;//value from hanso 1e-4;
       const Scalar c2 = 0.5; //value from hanso //value suggested in Numerical Optimiziation book = 0.9;
       Scalar a = 0;
-      Scalar b = std::numeric_limits<Scalar >::infinity();
+      Scalar b = std::numeric_limits<Scalar>::infinity();
       Scalar alpha = alpha_init; // important to try steplength one first
 
       // calculate initial values
       const Scalar f_init = objFunc.value(x);
       Scalar f = f_init;
-
       TVector grad(x.rows());
+
       objFunc.gradient(x, grad);
 
       const Scalar searchDirectionProjectedOnGradient_init = grad.dot(searchDir);
-      //assert(searchDirectionProjectedOnGradient_init >= 0 && "The search direction is not a descent direction.");
+      Scalar projectedGradAtAlpha;
 
       const Scalar sc1 = c1 * searchDirectionProjectedOnGradient_init;
       const Scalar sc2 = c2 * searchDirectionProjectedOnGradient_init;
@@ -50,60 +51,58 @@ namespace cppoptlib {
       // set parameters
       const Scalar searchDirNorm = searchDir.norm();
 
-      /*const Scalar nbisectmax = std::max(30, std::round(std::log2(1e5 * searchDirNorm))); //more iterations if ||d||
-      // big
-      const Scalar nexpandmax = std::max(10, std::round(std::log2(1e5 / searchDirNorm))); //more iterations if ||d|| small
-      Scalar nbisect = 0;
-      Scalar nexpand = 0;*/
+      const int nbisectmax = std::max(30, static_cast<int>(std::round(std::log2(1e5 * searchDirNorm))));
+      //more iterations if ||d|| is big
+      const int nexpandmax = std::max(10, static_cast<int>(std::round(std::log2(1e5 / searchDirNorm))));
+      //more iterations if ||d|| small
+      int nbisect = 0;
+      int nexpand = 0;
 
-      //bool done = false;
+      bool failed = false;
 
-
-      while (true) {
-
-        f = objFunc.value(x + alpha*searchDir);
-        objFunc.gradient(x + alpha*searchDir , grad);
+      while (!failed) {
+        f = objFunc.value(x + alpha * searchDir);
+        objFunc.gradient(x + alpha * searchDir, grad);
+        projectedGradAtAlpha = grad.dot(searchDir);
 
         // evaluate armijo condition (if not <=)
-        if ( f > f_init + alpha * sc1) b = alpha;
+        if ((f > f_init + alpha * sc1)
+            || (f == std::numeric_limits<Scalar>::quiet_NaN()) ) b = alpha;
           // evaluate weak Wolfe condition (if not >=)
-        else if ( grad.dot(searchDir) < sc2) a = alpha;
+        else if ((projectedGradAtAlpha < sc2)
+                 || (projectedGradAtAlpha == std::numeric_limits<Scalar>::quiet_NaN()) ) a = alpha;
         else {
           return alpha;
         }
 
-        // calculate new alpha
-      if (b < std::numeric_limits<Scalar>::infinity()) alpha = static_cast<Scalar>(0.5) * (a + b);
-        else alpha = static_cast<Scalar>(2.0) * a;
-
-        /*// calculate new alpha with stop criteria to prevent infinte bracketing
-        // calculate new alpha
-        if( b < std::numeric_limits::infinity() ) {
-          if( nbisect < nbisectmax ) {
+      // calculate new alpha with stop criteria to prevent infinte bracketing
+        if (b < std::numeric_limits<Scalar>::infinity()) {
+          if (nbisect < nbisectmax) {
             nbisect++;
-            alpha = 0.5 * (a+b);
-          }
-          else {
-            assert(false && "Line search failed to bracket point "
-                              "satisfying weak Wolfe conditions, "
-                              "function may be unbounded below.");
-            return alpha_init;
-          }
-        }
-        else {
-          if( nexpand < nexpandmax ) {
+            alpha = 0.5 * (a + b);
+          } else failed = true;
+        } else {
+          if (nexpand < nexpandmax) {
             nexpand++;
             alpha = 2.0 * a;
-
-          }
-          else {
-            assert(false && "Line search failed to satisfy weak "
-                              "Wolfe conditions although point "
-                              "satisfying conditions was bracketed.");
-            return alpha_init;
+          } else failed = true;
         }
-      }*/// calculate new alpha with stop criteria to prevent infinte bracketing
+        // simple method to calculate new alpha
+        /*if (b < std::numeric_limits<Scalar>::infinity()) alpha = static_cast<Scalar>(0.5) * (a + b);
+          else alpha = static_cast<Scalar>(2.0) * a;*/
       }
+
+      //TODO add as status
+      //if ( b == std::numeric_limits<Scalar>::infinity()) {
+      //  std::cout << "failed: minimizer never bracketed. "
+      //    "Function maybe unbounded below." << std::endl;
+      //}
+      //else {
+      //  std::cout << "failed: point satisfying Wolfe conditions was bracketed "
+      //    "but weak wolfe condition was not satisfied" << std::endl;
+      //}
+
+      return alpha;
     };
   };
 }
